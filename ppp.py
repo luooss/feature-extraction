3543549560345454dfeee9c68b85eee7b4123dd3
Oliver Luo <sjtu_luoshuai@sjtu.edu.cn>
2021-08-04 12:41:21 +0800

    luo

--- ppp.py
+++ ppp.py
@@ -1,16 +1,14 @@
-import copy
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
 
+cnt_file = r"D:\bcmi\exp\eeg_cnt_file\leixiaoting_20210324.cnt"
 
-a = ['1', '2', '3']
-b = a
-c = a[:]
-d = copy.copy(a)
-e = copy.deepcopy(a)
+EOG_channels = ['VEO', 'HEO']
+unused_channels = ['M1', 'M2']
 
-a.remove('1')
+raw = mne.io.read_raw_cnt(cnt_file, eog=EOG_channels, preload=True)
+raw.info['bads'].extend(EOG_channels)
+raw.info['bads'].extend(unused_channels)
 
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
